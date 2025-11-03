caf0bd5795076c3fb7a78f0082cf6adc6d70fe7d
vilar <spandeluispaulo@gmail.com>
2025-11-03 00:07:29 -0300

    Refatoramento: instanciando conexão na controller de passeios e passado para DAO

--- Projeto/proj_bga/src/test/java/com/example/proj_bga/view/PasseioView.java
+++ Projeto/proj_bga/src/test/java/com/example/proj_bga/view/PasseioView.java
@@ -1,6 +1,7 @@
 package com.example.proj_bga.view;
 
 import com.example.proj_bga.controller.PasseioController;
+import com.example.proj_bga.model.Passeio;
 import com.example.proj_bga.util.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,12 @@ public class PasseioView {
     private PasseioController passeioController;
 
     @PostMapping
-    public ResponseEntity<Object> addPasseio(@RequestParam("pde_descricao") String pde_descricao){
-        Map<String, Object> json = passeioController.addPass(pde_descricao);
+    public ResponseEntity<Object> addPasseio(@RequestBody Passeio  passeio) {
+        System.out.println(passeio);
+        Map<String, Object> json = passeioController.addPass(
+                passeio.getDescricao()
+        );
+
         if(json.get("erro") == null)
             return ResponseEntity.ok(new Mensagem("Passeio cadastrado com sucesso!"));
         return ResponseEntity.badRequest().body(new Mensagem(json.get("erro").toString()));
@@ -48,17 +53,17 @@ public class PasseioView {
     }
 
     @PutMapping
-    public ResponseEntity<Object> updatePasseio(@RequestParam("pde_id") int pde_id, @RequestParam("pde_descricao") String pde_descricao){
-        Map<String, Object> json = passeioController.updatePasseio(pde_id, pde_descricao);
-        if(json.containsKey("erro")) {
+    public ResponseEntity<Object> updatePasseio(@RequestBody Passeio passeio){
+        Map<String, Object> json = passeioController.updatePasseio(
+                passeio.getPdeId(),
+                passeio.getDescricao()
+        );
+
+        if (json.get("erro") == null) {
+            return ResponseEntity.ok(new Mensagem(json.containsKey("mensagem")? json.get("mensagem").toString(): "Passeio atualizado com sucesso!"));
+        }
+        else {
             return ResponseEntity.badRequest().body(new Mensagem(json.get("erro").toString()));
         }
-        else
-            if (json.containsKey("mensagem")) {
-                return ResponseEntity.ok(new Mensagem(json.get("mensagem").toString()));
-            }
-            else {
-                return ResponseEntity.ok(new Mensagem("Atualização realizada com sucesso."));
-            }
     }
 }
